921a001ce3b299b77840398a14607bc2558b6e55
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-02-11 14:41:34 +0100

    Kép mentése mappába

--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class uploadController extends Controller
 {
     public function upload(Request $request) {
-        $name = $request->file('picture')->store('pictures');
+        $name = $request->file('picture')->store('public/pictures');
         return response()->json(['filename' => $name]);
     }
 }
